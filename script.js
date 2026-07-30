d41d45739b34bf7802e85df4c246862b30c863b5
Mason Erickson <merickson6420@gmail.com>
2026-07-30 12:43:05 -0400

    Icons for each weather id are properly displayed.

--- script.js
+++ script.js
@@ -24,8 +24,11 @@ async function fetchWeatherData(city) {
         if (!hasError) {
             document.querySelector("#error-result").classList.add("hidden");
         }
+        console.log(data);
         document.querySelector(".location").textContent = data.name;
         document.querySelector(".temperature").textContent = `${Math.round((data.main.temp * 9/5) - 459.67)}°F`;
+        const iconCode = data.weather[0].id;
+        weatherIcon(iconCode);
         document.querySelector(".description").textContent = data.weather[0].description;
         document.querySelector(".humidity").textContent = `Humidity: ${data.main.humidity}%`;
         document.querySelector("#weather-result").classList.remove("hidden");
@@ -44,6 +47,21 @@ function handleError(message) {
     document.querySelector("#error-result").classList.remove("hidden");
 }
 
-function weatherIcon(temperature) {
-
+function weatherIcon(temp) {
+    const displayIcon = document.querySelector(".temperature-icon");
+    if (200 <= temp && temp < 532) {
+        displayIcon.innerHTML = '<i class="fa-solid fa-cloud-rain"></i>';
+    } else if (600 <= temp && temp < 623) {
+        displayIcon.innerHTML = '<i class="fa-solid fa-snowflake"></i>';
+    } else if (700 <= temp && temp < 782) {
+        displayIcon.innerHTML = '<i class="fa-solid fa-smog"></i>';
+    } else if (temp === 800) {
+        displayIcon.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    } else if (801 <= temp && temp < 803) {
+        displayIcon.innerHTML = '<i class="fa-solid fa-cloud-sun"></i>';
+    } else if (803 <= temp && temp < 805) {
+        displayIcon.innerHTML = '<i class="fa-solid fa-cloud"></i>';
+    } else {
+        displayIcon.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
+    }
 }
